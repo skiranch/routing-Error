2603b586d16b4d13bfe38d891d8d971e7f0be3da
skiranch <31719501+skiranch@users.noreply.github.com>
2017-10-30 10:35:06 +0530

    Modular Structure with Filter and Sass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit,EventEmitter,Output } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  displaystatus:boolean=false;
+  filterOption:string="all";
+  filterOptionStore:string="all";
+  @Output()
+  fileterBody:EventEmitter<string>=new EventEmitter<string>();
+  displayBody(status:boolean)
+  { 
+    this.displaystatus=!this.displaystatus;
+  }
+
+  myFilterBody(filterOption:any)
+  {
+    this.displaystatus=true;
+    this.filterOption=filterOption.opt1;
+    this.filterOptionStore=filterOption.opt2;
+    if(filterOption.opt1=='FILTER BY STATUS')
+    {
+      this.filterOption='all';
+    }
+    if(filterOption.opt2=='FILTER BY STORE')
+    {
+      this.filterOptionStore='all';
+    }
+  }
  }
--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-body',
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./body.component.scss','../app.component.scss']
 })
 export class BodyComponent implements OnInit {
+  @Input()
+  filterOption:string;
+
+  @Input()
+  filterOptionStore:string;
+
   
+  constructor() {
+   
+   }
    newImg:string;
   public myarray:Array<{headlabel:String,headlabel2:string,color:string,imglable:string,imglable2:string,stories:Array<{sname:string}>,sch:Array<{schlabel:string}>,imgarray:Array<{imgurl:string}>,boxdisplay:boolean}>=[
     {headlabel:'All Centres Gallery V2',headlabel2:'NOT PUBLISHED',color:'4px solid #ff9999',boxdisplay:false,imglable:'10 PRODUCTS',imglable2:'4 PUBLISHED',stories:[{sname:'Eastland Level 2'},{sname:'Robina Level 1'}],sch:[{schlabel:'Do Not Publish'}],imgarray:[{imgurl:'../assets/images/b8c8cea848343e9007e4c70093bd8996.jpg'},{imgurl:'../assets/images/71Uu3aPWtmL._UY445_.jpg'},{imgurl:'https://voguepk.com/wp-content/uploads/2017/03/Latest-Hairstyles-Fashion-Trends-for-Teen-Boys-3.jpg'},{imgurl:'https://static.pexels.com/photos/235462/pexels-photo-235462.jpeg'}]},
@@ -16,7 +25,9 @@ export class BodyComponent implements OnInit {
   ];
 
   addImage(newImg,myIndex)
-  { if(newImg==""){alert("Please Enter Image");}else{
+  {
+   
+    if(newImg==""){alert("Please Enter Image");}else{
      this.newImg=newImg;
      this.myarray[myIndex].imgarray.push({imgurl:newImg});
      this.myarray[myIndex].boxdisplay=!this.myarray[myIndex].boxdisplay;
@@ -27,8 +38,22 @@ export class BodyComponent implements OnInit {
     this.myarray[myIndex].boxdisplay=!this.myarray[myIndex].boxdisplay;
    
   }
-
-  constructor() { }
+  disStatus:boolean=false;
+  checkStore(filterOptionStore:string,stories:any):boolean
+  { this.disStatus=false;
+    for(let i=0;i<stories.length;i++)
+    { 
+      let sname2=stories[i].sname;
+     if(sname2.search(filterOptionStore)==-1)
+     {
+      
+     }else{
+      this.disStatus=true;
+     }
+    }
+  
+   return this.disStatus;
+  }
 
   ngOnInit() {
   }
